927da9e30447fc4800b5180473796fa382f52cf8
nworbnhoj <nworbnhoj@gmail.com>
2016-02-03 17:41:59 +1100

    travis no-sudo

--- travis-install.sh
+++ travis-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo
+apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo
