c7d995b0854df58a5af71e80797d0bf6c9414909
nworbnhoj <nworbnhoj@gmail.com>
2016-02-04 16:07:44 +1100

    reset

---
+++ brcm47xx-src/package/plugin-gargoyle-i18n-French-FR/files/www/i18n/French-FR/dhcp.js
@@ -0,0 +1,37 @@
+﻿/*
+ * UTF-8 (with BOM) French-FR text strings for dhcp.sh html elements
+ */
+
+dhcpS.SrvE="Serveur DHCP LAN activé";
+dhcpS.Drng="Plage DHCP";
+dhcpS.Strt="Début";
+dhcpS.End="Fin";
+dhcpS.LsTm="Durée du bail";
+dhcpS.SIPs="IP statiques";
+dhcpS.BlckM="Bloquer les adresses MAC qui se connectent avec une IP différente de celle attribuée";
+dhcpS.AdSIP="Ajouter une IP statique";
+dhcpS.SelH="Choisir Nom d&apos;hôte/MAC à partir de clients actuellement connectés";
+dhcpS.AsSIP="Adresses IP attribuées";
+dhcpS.opt="optionnel";
+dhcpS.ESIP="Éditer l&apos;IP statique";
+dhcpS.dHErr="doublon de nom d&apos;hôte";
+dhcpS.dMErr="doublon d&apos;adresse MAC";
+dhcpS.dIPErr="doublon d&apos;adresse IP";
+dhcpS.subErr="L&apos;adresse IP statique spécifiée est en dehors du sous-réseau LAN.";
+dhcpS.ipErr="L&apos;adresse IP statique spécifiée est celle du routeur.";
+dhcpS.dsubErr="La plage DHCP spécifiée est en dehors du sous-réseau LAN.";
+dhcpS.dipErr="La plage DHCP spécifiée content l&apos;IP du LAN.";
+dhcpS.upErr="Impossible de mettre à jour l&apos;IP statique.";
+dhcpS.AErr="Impossible d&apos;ajouter une colonne.";
+
+dhcpS.KnDev="Equipements connus";
+dhcpS.AdKnDev="Ajouter un équipement connu";
+dhcpS.AdGp="Ajouter un groupe";
+dhcpS.GpNm="Nom du groupe";
+dhcpS.DevNm="Nom de l&apos;équipement";
+dhcpS.DevNms="Noms des équipements";
+dhcpS.EdKnDev="Editer l&apos;équipement";
+dhcpS.DevGp="Groupes d&apos;équipement";
+dhcpS.SelM="Inclure l&apos;adresse MAC";
+dhcpS.SelD="Inclure l&apos;équipement";
+dhcpS.StcIP="IP statique";
